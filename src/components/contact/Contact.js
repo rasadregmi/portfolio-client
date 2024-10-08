8de04532a5a625775ff75870fdeb05bd93e78e1c
Rasad Regmi <regmirasad53@gmail.com>
2024-10-08 20:22:42 +0545

    server path changed

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -35,7 +35,7 @@ const Contact = () => {
       setErrMsg("Message is required!");
     } else {
       try {
-        const response = await fetch('http://localhost:5000/api/contact', {
+        const response = await fetch('https://portfolio-server-snowy-ten.vercel.app/api/contact', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
