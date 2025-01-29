af90e60cd5ddc263656cd7a15f337c2b6fb2834b
Rasad Regmi <regmirasad53@gmail.com>
2025-01-29 14:22:20 +0545

    Image updated

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from "react";
 import { bannerImg } from "../../assets/index";
 
 const RightBanner = () => {
   return (
     <div className="w-full lgl:w-1/2 flex justify-center lgl:justify-end items-center relative">
       <img
-        className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[580px] z-10"
+        className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[580px] z-10 rounded-2xl"
         src={bannerImg}
         alt="bannerImg"
       />
-      </div>
+    </div>
   );
-}
+};
 
-export default RightBanner
+export default RightBanner;
