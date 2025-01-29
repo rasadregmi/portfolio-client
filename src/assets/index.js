d9a21d79063dbe74c5014b4ef81c930be6cc2c3c
Rasad Regmi <regmirasad53@gmail.com>
2025-01-29 16:02:06 +0545

    Contact page updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,7 +5,7 @@ import projectTwo from "./images/projects/projectTwo.png";
 import projectThree from "./images/projects/projectThree.png";
 import projectFour from "./images/projects/projectFour.png";
 import quote from "./images/testmonial/quote.png";
-import contactImg from "./images/contact/contactImg.png";
+import contactImg from "./images/bannerImg.png";
 
 export {
   logo,
--- src/components/contact/ContactLeft.js
+++ src/components/contact/ContactLeft.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { FaFacebookF, FaInstagramSquare, FaLinkedinIn } from "react-icons/fa";
 import { contactImg } from "../../assets/index";
 
 const ContactLeft = () => {
   return (
-    <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-4 lgl:p-8 rounded-lg shadow-shadowOne flex flex-col gap-8 justify-center">
+    <div className="w-full lgl:w-[35%] bg-gradient-to-r from-[#1e2024] to-[#23272b] p-4 lgl:p-8 rounded-lg shadow-shadowOne flex flex-col gap-8 justify-center">
       <img
-        className="w-full h-64 object-cover rounded-lg mb-2"
+        className="w-full object-cover mb-2"
         src={contactImg}
         alt="contactImg"
       />
@@ -24,26 +23,6 @@ const ContactLeft = () => {
         <p className="text-base text-gray-400 flex items-center gap-2">
           Email: <span className="text-lightText">regmirasad53@gmail.com</span>
         </p>
-      </div>
-      <div className="flex flex-col gap-4">
-        <h2 className="text-base uppercase font-titleFont mb-4">Find me in</h2>
-        <div className="flex gap-4">
-          <a href="https://www.facebook.com/rasad.regmi" target="blank">
-            <span className="bannerIcon" >
-              <FaFacebookF />
-            </span>
-          </a>
-          <a href="https://www.instagram.com/regmi_rasad/" target="blank">
-            <span className="bannerIcon">
-              <FaInstagramSquare />
-            </span>
-          </a>
-          <a href="https://www.linkedin.com/in/rasad-regmi/" target="blank">
-            <span className="bannerIcon">
-              <FaLinkedinIn />
-            </span>
-          </a>
-        </div>
       </div>
     </div>
   );
