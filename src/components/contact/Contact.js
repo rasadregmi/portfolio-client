49680e11e8e77cb79c7b0e2c0b5dff3c1f6972b4
Rasad Regmi <regmirasad53@gmail.com>
2025-02-02 17:51:33 +0545

    Contact updated

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -69,7 +69,7 @@ const Contact = () => {
   return (
     <section
       id="contact"
-      className="w-full py-20 border-b-[1px] border-b-black"
+      className="w-full py-24 border-b-[1px] border-b-black"
     >
       <div className="flex justify-center items-center text-center">
         <Title title="CONTACT" des="Connect With Me" />
