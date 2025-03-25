1b34abfcb6eea5394dac4f2505250a5b3a31f4d3
Rasad Regmi <regmirasad53@gmail.com>
2025-03-25 11:46:38 +0545

    Experience Updating

--- src/components/resume/Experience.js
+++ src/components/resume/Experience.js
@@ -8,15 +8,9 @@ const Experience = () => {
       animate={{ opacity: 1, transition: { duration: 0.5 } }}
       className="w-full flex flex-col lgl:flex-row gap-10 lgl:gap-20"
     >
-      <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-        <p className="text-base md:text-lg font-sans leading-relaxed">
-          Although I do not have formal work experience, my academic coursework and personal projects have equipped me with valuable skills and knowledge. I have a solid foundation in <strong>web development</strong>, particularly in the <strong>MERN stack & Next.js</strong>.
-        </p>
-        <p className="text-base md:text-lg font-sans leading-relaxed">
-          I have actively participated in <strong>MBMC IdeaX 2024</strong>, <strong>Orbit Engineering Hacks 2024</strong>, and <strong>Noskathon Lite 2024</strong>, where my team (Team Aegis) and I developed several projects. Through these experiences, I gained hands-on expertise in <strong>full-stack development</strong> and <strong>AI/ML</strong>, collaborated with a team to solve real-world problems, and learned to work effectively under time constraints.
-        </p>
-        <p className="text-base md:text-lg font-sans leading-relaxed">
-          Additionally, I contributed to <strong>ClassLens</strong>, an AI-powered classroom attentiveness monitoring system that analyzes student engagement using <strong>machine learning</strong>, and <strong>Traffic Dai</strong>, a real-time <strong>traffic-based travel assistant</strong> optimized for Nepal’s unique road conditions. These experiences have strengthened my <strong>problem-solving abilities, teamwork, and proficiency</strong> in <strong>Python, Java, C++, and ML frameworks (TensorFlow, Scikit-learn)</strong>.
+      <div className="py-20 font-titleFont flex flex-col gap-6 items-center">
+        <p className="text-lg md:text-xl font-sans leading-relaxed font-semibold text-gray-200">
+          Updating soon...
         </p>
       </div>
     </motion.div> 
